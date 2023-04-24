58eb9a2b692e4b0c8add1dd522f9da44ae2cf63e
Perdition <87424475+Perdition-117@users.noreply.github.com>
2023-04-24 22:00:30 +0200

    Add class for reading paths file

---
+++ LibMeido/Paths.cs
@@ -0,0 +1,25 @@
+﻿namespace LibMeido;
+
+public class Paths {
+	private const string FileHeader = "CM3D2_PATHS";
+
+	public static IEnumerable<string> ReadPathFile(string filePath) {
+		using var fileStream = File.OpenRead(filePath);
+		using var binaryReader = new BinaryReader(fileStream);
+
+		var header = binaryReader.ReadString();
+		if (header != FileHeader) {
+			throw new Exception($"Invalid header \"{header}\" for paths file. Expected \"{FileHeader}\".");
+		}
+
+		var version = binaryReader.ReadInt32();
+
+		var numPaths = binaryReader.ReadInt32();
+		for (var i = 0; i < numPaths; i++) {
+			var text = binaryReader.ReadString();
+			yield return text;
+		}
+
+		yield break;
+	}
+}
